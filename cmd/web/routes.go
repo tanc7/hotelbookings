6f5a80e3c5cb948e0bb853cbb3359d025ee11ee1
Slayer Ranger <tanc7@unlv.nevada.edu>
2022-05-13 14:56:39 -0700

    Let's not advertise a creepy serial killer motel and rather, a top of the line 5 star hotel

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -19,6 +19,8 @@ func routes(app *config.AppConfig) http.Handler {
 	//mux.Use(WriteToConsole)
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	fileserver := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileserver))
 
 	return mux
 }
